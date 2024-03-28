c0ed752ab40dae6b4476cca2c11b5ddb7d17196f
Robert Chandler <robert.chandler@elationhealth.com>
2024-03-28 11:32:54 -0700

    Restore luasnip

--- init.lua
+++ init.lua
@@ -59,6 +59,9 @@ require("lazy").setup({
     -- Autocompletion
     "hrsh7th/nvim-cmp",
     dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
       -- Adds LSP completion capabilities
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-path",
@@ -617,6 +620,9 @@ mason_lspconfig.setup_handlers({
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
 local cmp = require("cmp")
+local luasnip = require("luasnip")
+require("luasnip.loaders.from_vscode").lazy_load()
+luasnip.config.setup({})
 
 cmp.setup({
   completion = {
